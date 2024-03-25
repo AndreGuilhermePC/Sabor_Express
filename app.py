a90d80e498b927f0ffda21f3783bbabce88640d7
Andre <Andre@gmail.com>
2024-03-24 21:04:59 -0300

    Projeto finalizado

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 
-lista_de_restalrante = [{'Nome': 'Bom prato', 'Categoria':'Comida Brasileira', 'Status':'Ativo'},
-                        {'Nome': 'Bar dos irmãos', 'Categoria':'Bar', 'Status':'Inativo'},
-                        {'Nome': 'Bela Pizza', 'Categoria':'Comida Italiana', 'Status':'Ativo'}]
+lista_de_restalrante = [{'nome': 'Bom prato', 'categoria':'comida brasileira', 'status':'ativo'},
+                        {'nome': 'Bar dos irmãos', 'categoria':'bar', 'status':'inativo'},
+                        {'nome': 'Bela Pizza', 'categoria':'comida italiana', 'status':'ativo'}]
 
 def exibir_nome_do_programa():
     print("""
@@ -17,7 +17,7 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado do restaurante')
     print('4. Sair\n')
 
 def voltar_ao_menu_principal():
@@ -38,10 +38,19 @@ def opcao_invalida():
     voltar_ao_menu_principal()
 
 def cadastrar_novo_restalrante():
+    '''Essa função é responsavel por cadastrar um novo restaurante
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+    Output:
+    - Adiciona um novo restaurante a lista de restaurantes
+    '''
     exibir_subtitulo('Cadastro de novos restaurantes')
-
+    
     nome_do_restalrante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    lista_de_restalrante.append(nome_do_restalrante)
+    solicita_categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restalrante}: ')
+    dados_do_restaurante = {'nome':nome_do_restalrante, 'categoria':solicita_categoria, 'status':False}
+    lista_de_restalrante.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restalrante} foi cadastrado com sucesso!\n')
     
     voltar_ao_menu_principal()
@@ -50,8 +59,28 @@ def lista_restaurante():
     exibir_subtitulo('Listando os restalrantes')
     for restaurante in lista_de_restalrante:
         #nome_restaurante = 
-        print(f'- {restaurante['Nome']} | {restaurante['Categoria']} | {restaurante['Status']}')
+        print(f'- {restaurante['nome']} | {restaurante['categoria']} | {restaurante['status']}')
     
+    voltar_ao_menu_principal()
+
+def alternar_estado_do_restaurante():
+    '''Essa função é responsavel por alterar o estado do restaurante
+    Inputs:
+    - Nome do restaurante
+    Outout:
+    - A confirmação de que foi ativo, se foi desativado e se esta na lista       
+    '''
+    exibir_nome_do_programa('Alternar estado do restaurante')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+    for restaurante in lista_restaurante:
+        if nome_do_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_do_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_do_restaurante} foi desativado com sucesso'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')   
     voltar_ao_menu_principal()
 
 def escolher_opcoes():
@@ -62,7 +91,7 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             lista_restaurante()
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            alternar_estado_do_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
